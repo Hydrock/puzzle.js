87dc21f49223a03f6f85d5eda98b244e30b87b18
Hydrock <hydrock74@gmail.com>
2017-10-31 14:10:40 +0300

    add create config function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony
+var chalk = require('chalk'); // color console
 let co = require('co');
-let prompt = require('co-prompt');
+let prompt = require('prompt');
 let commander = require('commander');
 const path = require('path');
 var fs   = require('fs');
@@ -8,15 +9,37 @@ var lib  = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
 
 let pathStartProgramm = process.cwd();
 
-let pathProject = '/home/avechkanov/Documents/Web/puzzle test folder';
-
 let projectConfig;
 
 try {
-  projectConfig = require(pathProject + '/config.json');
+  projectConfig = require(pathStartProgramm + '/config1.json');
 } catch (error) {
-  console.log('Config do not finded!');
-  return;
+  console.log(chalk.red('Config do not finded!'));
+  createConfig()
+  //return;
 }
 
-console.log(projectConfig)
+function createConfig () {
+  let config = {x: 11};
+
+  prompt.start();
+  prompt.get(['name'], function (err, result) {
+    console.log('Command-line input received:');
+    console.log('  Project Name: ' + result.name);
+
+    config = {name: result.name}
+    config = JSON.stringify(config);
+    writeConfig(config)
+  });
+}
+
+function writeConfig (dataObj) {
+  var json = JSON.stringify(dataObj);
+
+  fs.writeFile("./config.json", json, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log(chalk.green("The config was saved!"));
+  }); 
+}
