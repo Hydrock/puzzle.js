439ed713e95d293dde124701f330127c816c2b02
Alex Vechkanov <hydrock74@gmail.com>
2017-11-01 14:48:35 +0300

    firs http res

---
+++ app/server/routing/index.js
@@ -0,0 +1,61 @@
+const url = require('url');
+const fs = require('fs');
+
+const define = function(req, res, postData) {
+
+if(/\.js$/gi.test(req.url)) {
+  console.log('js');
+  res.writeHead(200, {
+    'Content-Type': 'application/javascript'
+  });
+  let readStream = fs.createReadStream(prePath+'/../../client/js/app.bundle.js');
+  readStream.pipe(res);
+  return;
+} else {
+
+    const urlParsed = url.parse(req.url, true);
+    let path = urlParsed.pathname;
+    prePath = __dirname;
+
+    let filePath = prePath+'/../../client/index.html';
+
+
+    fs.readFile(filePath, 'utf-8', (err, html) => {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(html);
+    })
+
+    /*
+    try {
+      let dynPath = './dynamic/' + path;
+      let routeDestination = require(dynPath);
+      res.end('We have API!');
+    }
+    catch (err) {
+      let filePath = prePath+'/static'+path+'/index.html';
+        fs.readFile(filePath, 'utf-8', (err, html) => {
+          if(err) {
+            let nopath = prePath + '/nopage/index.html';
+            fs.readFile(nopath, (err , html) => {
+              if(!err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end(html);
+              }
+              else{
+                let text = "Something went wrong. Please contact webmaster@forgetable.ru";
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end(text);
+              }
+            });
+          } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(html);
+          }
+        })
+    }
+    */
+};
+
+}
+
+exports.define = define;
--- src/server/routing/index.js
+++
@@ -1,4 +0,0 @@
-const define = function(req, res, postData) {
-  res.end('Hello, Habrahabr!');
-}
-exports.define = define;
